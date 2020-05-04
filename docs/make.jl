ee6dfbd1d3c45341bc19dda9e31e9bb34a1d9187
macorstanje <macorstanje@gmail.com>
2020-05-04 09:40:46 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,39 @@
 using Documenter
+Documenter.generate("Manifolds")
 
 include("../src/Manifolds.jl")
 using .Manifolds
+
+# makedocs(
+#     modules = [Manifolds],
+#     sitename = "Manifolds.jl",
+#     authors = "Marc Corstanje and contributors",
+#     pages = Any[
+#         "Home" => "index.md",
+#         "Manual" => "manual.md",
+#         "Library" => "library.md",
+#     ],
+# )
 
 makedocs(
     modules = [Manifolds],
     sitename = "Manifolds.jl",
     authors = "Marc Corstanje and contributors",
-    pages = Any[
-        "Home" => "index.md",
-        "Manual" => "manual.md",
-        "Library" => "library.md",
-    ],
+    doctest = false,
+    pages = Any[ # Compat: `Any` for 0.4 compat
+    "Home" => "index.md",
+    "Manual" => "manual.md",
+    "Library" => Any[
+        "Basics" => "library/basics.md",
+        "Geodesics" => "library/geodesics.md",
+        "Frames and the frame bundle" => "library/frames.md"
+        ]
+    ]
 )
 
 deploydocs(
-    repo = "github.com/macorstanje/Manifolds.jl.git"
+    repo = "github.com/macorstanje/Manifolds.jl.git",
+    target = "build",
+    push_preview = true,
+    versions = ["stable" => "v^", "v#.#", "dev" => "master"],
 )
