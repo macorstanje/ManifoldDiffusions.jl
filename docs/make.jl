e5139c6521c2a8214b3f1ec8adaf1e01af057613
macorstanje <macorstanje@gmail.com>
2020-05-07 20:40:38 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,19 +8,22 @@ makedocs(
     sitename = "ManifoldDiffusions.jl",
     authors = "Marc Corstanje and contributors",
     doctest = false,
+    format = :html,
     pages = Any[ # Compat: `Any` for 0.4 compat
     "Home" => "index.md",
     "Manual" => "manual.md",
     "Library" => Any[
         "Basics" => "library/basics.md",
         "Geodesics" => "library/geodesics.md",
-        "Frames and the frame bundle" => "library/frames.md"
+        "Frames and the frame bundle" => "library/frames.md",
+        "Plots on Manifolds" => "library/manifoldplots.md"
         ]
     ]
 )
 
 deploydocs(
     repo = "github.com/macorstanje/ManifoldDiffusions.jl.git",
+    branch = "gh-pages",
     deps=nothing,
     make=nothing
 )
